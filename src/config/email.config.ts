9be4d778db873b9a444d8275e0accc4c5c230b12
Plouski <gervaisines@gmail.com>
2025-03-26 10:16:23 +0100

    code push

--- src/config/email.config.ts
+++
@@ -1,3 +0,0 @@
-export const emailConfig = {
-  templateDir: process.env.EMAIL_TEMPLATE_DIR || 'src/templates/email',
-};
--- src/config/firebase.config.ts
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
-export const firebaseConfig = {
-    projectId: process.env.FIREBASE_PROJECT_ID || 'your_project_id',
-    privateKey: process.env.FIREBASE_PRIVATE_KEY || 'your_private_key',
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL || 'your_client_email',
-    databaseURL: process.env.FIREBASE_DATABASE_URL || '',
-};
